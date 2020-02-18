8dc61364d57b76ef9e4f5808d1ddca6ae5028fa5
sink666 <er.basilisk@gmail.com>
2020-02-18 02:47:26 -0500

    added boomer spacing and comma splicing functions

--- empress.rkt
+++ empress.rkt
@@ -1,10 +1,21 @@
 #! /usr/bin/env racket
 #lang racket
 
+(define (boomerspaces line)
+  (string-replace line " " "  "))
+
+(define (comma-splice line)
+  (cond
+    [(and (not (string-contains? line ".\n"))
+          (string-contains? line ".")
+          (string-replace line "." ","))]
+    [else line]))
+
 (define (next-line-in-file file)
   (let ((line (read-line file 'any)))
     (unless (eof-object? line)
-      (printf line)
+      (printf (boomerspaces
+               (comma-splice line)))
       (next-line-in-file file))))
 
 (define (empress fname)
