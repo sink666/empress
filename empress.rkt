549b63021528825e78c5a0ca8fa1d0d7443b6120
sink666 <er.basilisk@gmail.com>
2020-05-08 19:04:16 -0600

    bunch of major refactoring

--- empress.rkt
+++ empress.rkt
@@ -1,39 +1,58 @@
 #! /usr/bin/env racket
 #lang racket
 
-(define (boomerspaces line)
-  (string-replace line " " "  "))
-
-(define (comma-splice line)
-  (let ((splice? (random 2)))
+;;improve spacing
+(define (alter-spacing line)
+  (let ((booming? (random 2)))
     (cond
-      [(and (not (string-contains? line ".\n"))
-            (string-contains? line ".")
-            (equal? splice? 1))
-       (string-replace line "." ",")]
-      [else line])))
+      ((equal? booming? 1)
+       (string-replace line " " "  "))
+      (else (string-replace line "." ". ")))))
 
-(define (addatmosphere line)
-  (let ((needsatmos? (random 2)))
+;;check for line breaks and places for splices
+(define check-line
+  (lambda (line)
+    (condz
+      ((and (not (string-contains? line ".\n"))
+           (string-contains? line "."))))))
+
+;;introduce comma splices and more atmospheric comma splices
+(define improve-grammar
+  (lambda (line)
+    (let ((splice? (random 2)) (atmos-splice? (random 2)))
+      (cond
+        ((and (check-line line)
+              (equal? atmos-splice? 1))
+         (string-replace line "." "..."))
+        ((and (check-line line)
+              (eof-object? line)
+              (equal? splice? 1))
+         (string-replace line "." ","))
+        (else line)))))
+
+(define process-line
+  (lambda (line)
+    (improve-grammar (alter-spacing (string-append line "\n")))))
+
+(define enhance-file
+  (lambda (fn)
+    (let ((line (read-line fn 'any)))
+      (unless (eof-object? line)
+        (display (process-line line))
+        (enhance-file fn)))))
+
+;;add filename suggestions to the last else?
+(define empress
+  (lambda (cla)
     (cond
-      [(equal? needsatmos? 1)
-       (string-replace line "." "...")]
-      [else line])))
+      ((vector-empty? cla) (printf "err; no filename given"))
+      (else
+       (let ((fn (vector-ref cla 0)))
+         (cond
+           ((file-exists? fn) (call-with-input-file fn enhance-file))
+           (else (printf "err; no file with name ~a" fn))))))))
 
-(define (next-line-in-file file)
-  (let ((line (read-line file 'any)))
-    (unless (eof-object? line)
-      (display (boomerspaces
-                (comma-splice
-                 (addatmosphere
-                 (string-append line "\n")))))
-      (next-line-in-file file))))
-
-(define (empress fname)
-  (cond
-    [(file-exists? fname) (call-with-input-file fname next-line-in-file)]
-    [else printf "no such file to enhance"]))
-
-(empress (car (vector->list (current-command-line-arguments))))
+(printf "enhancing file!\n\n")
+(empress (current-command-line-arguments))
 
 ;;eof
